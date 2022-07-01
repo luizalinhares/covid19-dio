cf199187ff265670ca86e77a194251524e1e9729
luizalinhares <luizaclinhares@hotmail.com>
2022-06-30 23:52:32 -0300

    pwa init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { StyledEngineProvider } from '@mui/styled-engine-sc'
+import { CssBaseline } from '@mui/material'
+import GlobalStyle from './commons/styles/global-style'
+import Main from './containers/Main'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<StyledEngineProvider injectFirst>
+<CssBaseline />
+<GlobalStyle />
+<Main />
+</StyledEngineProvider>
   );
 }
 
---
+++ src/components/Skeleton.jsx
@@ -0,0 +1,3 @@
+import Skeleton from '@mui/lab/Skeleton'
+
+export default Skeleton
